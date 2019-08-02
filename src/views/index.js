78686c7c918ecb943d9ef6678e733f0001e6c4d6
YeonwooSung <neos960518@gmail.com>
2019-08-02 16:36:44 +0900

    Change comfort zone range

--- src/views/index.js
+++ src/views/index.js
@@ -9,44 +9,52 @@ const Y_offset = 0.62198;
 const height_offset = 0.0000225577;
 const height_exp = 5.25588;
 
-const layoutHumi1 = 40;
-const layoutHumi2 = 60;
+// comfort zone - humidity
+let comfortZoneHumi1 = 40;
+let comfortZoneHumi2 = 60;
 
-function getBorderString(layout_xValues1, layout_yValues1, layout_xValues2, layout_yValues2) {
-    let str = ' M ' + layout_xValues1[0] + ' ' + layout_yValues1[0];
+// Graph axes ranges
+let xRange_start = 10;
+let xRange_end = 50;
+let yRange_start = 0;
+let yRange_end = 0.06;
 
-    for (var i = 1; i < layout_xValues1.length; ++i) {
+
+function getBorderString(comfort_zone_xValues1, comfort_zone_yValues1, comfort_zone_xValues2, comfort_zone_yValues2) {
+    let str = ' M ' + comfort_zone_xValues1[0] + ' ' + comfort_zone_yValues1[0];
+
+    for (var i = 1; i < comfort_zone_xValues1.length; ++i) {
         str += ' L'
-        str += layout_xValues1[i];
+        str += comfort_zone_xValues1[i];
         str += ' '
-        str += layout_yValues1[i];
+        str += comfort_zone_yValues1[i];
     }
 
-    for (var i = layout_xValues2.length - 1; i >= 0; --i) {
+    for (var i = comfort_zone_xValues2.length - 1; i >= 0; --i) {
         str += ' L'
-        str += layout_xValues2[i];
+        str += comfort_zone_xValues2[i];
         str += ' '
-        str += layout_yValues2[i];
+        str += comfort_zone_yValues2[i];
     }
 
     str += ' Z'
     return str;
 }
 
-function getLayoutForComfortRegion(layout_xValues1, layout_yValues1, layout_xValues2, layout_yValues2) {
-    let str = getBorderString(layout_xValues1, layout_yValues1, layout_xValues2, layout_yValues2);
+function getLayoutForComfortRegion(comfort_zone_xValues1, comfort_zone_yValues1, comfort_zone_xValues2, comfort_zone_yValues2) {
+    let str = getBorderString(comfort_zone_xValues1, comfort_zone_yValues1, comfort_zone_xValues2, comfort_zone_yValues2);
 
     var layout = {
         title: 'Thermal Comfort Zone',
         xaxis: {
-            range: [0, 50],
+            range: [xRange_start, xRange_end],
             zeroline: false
         },
         yaxis: {
             range: [0, 0.06],
             showgrid: false
         },
-        width: 700,
+        width: 1200,
         height: 700,
         shapes: [
             {
@@ -91,65 +99,75 @@ function draw(t, f, h) {
         const yValues9 = xValues.map((x) => { return calculateY(x, 90, h); });
         const yValues10 = xValues.map((x) => { return calculateY(x, 100, h); });
 
-        const layout_xValues1 = math.range(20, 30, 0.5).toArray();
-        const layout_xValues2 = math.range(20, 30, 0.5).toArray();
-        const layout_yValues1 = layout_xValues1.map((x) => { return calculateY(x, layoutHumi1, h); });
-        const layout_yValues2 = layout_xValues2.map((x) => { return calculateY(x, layoutHumi2, h); });
+        const comfort_zone_xValues1 = math.range(22, 25, 0.5).toArray();
+        const comfort_zone_xValues2 = math.range(22, 25, 0.5).toArray();
+        const comfort_zone_yValues1 = comfort_zone_xValues1.map((x) => { return calculateY(x, comfortZoneHumi1, h); });
+        const comfort_zone_yValues2 = comfort_zone_xValues2.map((x) => { return calculateY(x, comfortZoneHumi2, h); });
 
         // render the plot using plotly
         const trace1 = {
             x: xValues,
             y: yValues1,
+            name: 'humidity 10%',
             type: 'scatter'
         }
 
         const trace2 = {
             x: xValues,
             y: yValues2,
+            name: 'humidity 20%',
             type: 'scatter'
         }
         const trace3 = {
             x: xValues,
             y: yValues3,
+            name: 'humidity 30%',
             type: 'scatter'
         }
         const trace4 = {
             x: xValues,
             y: yValues4,
+            name: 'humidity 40%',
             type: 'scatter'
         }
         const trace5 = {
             x: xValues,
             y: yValues5,
+            name: 'humidity 50%',
             type: 'scatter'
         }
         const trace6 = {
             x: xValues,
             y: yValues6,
+            name: 'humidity 60%',
             type: 'scatter'
         }
         const trace7 = {
             x: xValues,
             y: yValues7,
+            name: 'humidity 70%',
             type: 'scatter'
         }
         const trace8 = {
             x: xValues,
             y: yValues8,
+            name: 'humidity 80%',
             type: 'scatter'
         }
         const trace9 = {
             x: xValues,
             y: yValues9,
+            name: 'humidity 90%',
             type: 'scatter'
         }
         const trace10 = {
             x: xValues,
             y: yValues10,
+            name: 'humidity 100%',
             type: 'scatter'
         }
 
-        const layout = getLayoutForComfortRegion(layout_xValues1, layout_yValues1, layout_xValues2, layout_yValues2);
+        const layout = getLayoutForComfortRegion(comfort_zone_xValues1, comfort_zone_yValues1, comfort_zone_xValues2, comfort_zone_yValues2);
 
         const data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8, trace9, trace10]
         Plotly.newPlot('plot', data, layout)
